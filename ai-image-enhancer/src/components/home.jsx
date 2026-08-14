19df920f1a8cbad3b892afa4875f7c529e8e10d7
umm-written <amritbiswas612@gmail.com>
2026-08-14 19:45:16 +0530

    done with home-page functionality.

--- ai-image-enhancer/src/components/home.jsx
+++ ai-image-enhancer/src/components/home.jsx
@@ -1,13 +1,36 @@
 import ImageUpload from "./ImageUpload";
 import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi"
 
 const Home = () => {
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
+    
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try agaain later.")
+        }
+    };
+
     return (
-        <div className="">
-            <ImageUpload />
-            <ImagePreview />
-        </div>
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler}/>
+            <ImagePreview 
+                uploadImage={uploadImage}
+                enhancedImage={enhancedImage?.image}
+                loading={loading}
+            />
+        </>
     );
 };
 
-export default Home
+export default Home;
