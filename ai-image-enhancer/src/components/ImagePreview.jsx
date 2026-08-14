939eb590d21a172d5727df74e54ab0ab40a15ffb
umm-written <amritbiswas612@gmail.com>
2026-08-14 22:09:33 +0530

    built the image-preview functionality.

--- ai-image-enhancer/src/components/ImagePreview.jsx
+++ ai-image-enhancer/src/components/ImagePreview.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import Loading from "./Loading";
 
-const ImagePreview = () => {
+const ImagePreview = (props) => {
     return (
-        <div>
-            <h1>ImagePreview</h1>
+        <div className="mt-8 grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
+            {/* Original Image */}
+            <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+                <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">
+                    Original Image
+                </h2>
+
+                {props.uploaded ? (
+                    <img
+                    src={props.uploaded}
+                    alt="Uploaded"
+                    className="w-full h-full object-cover"
+                />
+                ) : (
+                    <div className="flex items-center justify-center h-80 bg-gray-200">
+                        No Image Selected.
+                    </div>
+                )}
+            </div>
+
+            {/* Enhanced Image */}
+            <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+                <h2 className="text-xl font-semibold text-center bg-blue-800 text-white py-2">
+                    Enhanced Image
+                </h2>
+
+                {props.enhanced && !props.loading && (
+                    <img
+                        src={props.enhanced}
+                        alt="Enhanced"
+                        className="w-full h-full object-cover"
+                    />
+                )}
+
+                {props.loading ? (
+                    <Loading />
+                ) : (
+                    <div className="flex items-center justify-center h-80 bg-gray-200">
+                        No Enhanced Image.
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
 
-export default ImagePreview
+export default ImagePreview;
