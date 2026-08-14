cc9187b67ac17f67b11eb69721a83fa0b6b190cd
umm-written <amritbiswas612@gmail.com>
2026-08-14 22:17:05 +0530

    built the loading functionality.

--- ai-image-enhancer/src/components/Loading.jsx
+++ ai-image-enhancer/src/components/Loading.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className='flex justify-center items-center h-full'>
+      <div className='animate-spin border-4 border-t-transparent w-10 h-10 scale-200 rounded-full border-red-300'></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
