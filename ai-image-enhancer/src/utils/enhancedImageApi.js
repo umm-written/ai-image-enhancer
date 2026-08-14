edfb5d19cd8a7b58cf1821cb89e1a57176c5d286
umm-written <amritbiswas612@gmail.com>
2026-08-15 01:19:46 +0530

    connected local frontend to deployed backend.

--- ai-image-enhancer/src/utils/enhancedImageApi.js
+++ ai-image-enhancer/src/utils/enhancedImageApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://ai-image-enhancer-py6u.onrender.com";
 
 export const enhancedImageAPI = async (file) => {
     try {
